f76720b5fbcb782f6dc75ec5804a688e437914b5
Marc Ràfols Ibáñez <marc@marcrafols.com>
2023-05-24 13:55:53 +0200

    changes

--- script.js
+++
@@ -1,8 +0,0 @@
-// Script for handling the burger menu toggle
-window.addEventListener('DOMContentLoaded', function () {
-    var menuToggle = document.getElementById('menu-toggle');
-    menuToggle.addEventListener('change', function () {
-      document.body.classList.toggle('menu-open');
-    });
-  });
-  
